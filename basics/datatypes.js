ad5aefb90f1531f2c5a00df321f907d58db499c2
Raunak11-tandon <raunaktandon11@gmail.com>
2026-06-13 11:09:37 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"// treats all code as the new version of js 
+ 
+//alert( 3+3) // we are using node js not browser
+ 
+console.log(3+3);  
+console.log("raunak") // code readability should be high
+
+let name="raunak" 
+let age=20
+let loggedin=true 
+let state; 
+//let state=null  empty 
+// number => 2 to the power 53 
+//BigInt 
+//boolean either true or false 
+// null- standalone value 
+// undefined - value is not assigned till now  
+// null and undefined are two different things in js  null  means empty and undefined meanss any value is not assigned to the value
+ // symbol > uniqueness 
+ // object 
+ console.log(typeof "raunak")
+ console.log(typeof null)
+ console.log(typeof undefined)

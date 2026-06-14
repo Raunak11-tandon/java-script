e2d3ad6f092783067f302445f0b790bb1c5cea6f
Raunak11-tandon <raunaktandon11@gmail.com>
2026-06-14 08:07:50 +0000

    datatype  conversion

---
+++ basics/conversion.js
@@ -0,0 +1,33 @@
+let score="raunak"
+
+//console.log(typeof score)
+//console.log(typeof (score))  
+
+let valueInNumber=Number(score)
+//console.log(typeof (valueInNumber))  
+//console.log((valueInNumber))   
+
+// conerting a number easily convert to 33   
+//"33"=33
+//if "33abc" cannot convert so give output NaN
+//"33abc"=NaN
+// if true convert to 1 and false convert to 0
+
+
+
+let loggedIn="raunak"
+let booleanloggedIn=Boolean(loggedIn)
+//console.log(booleanloggedIn)
+//1=> true 
+// 0=>false 
+// " "=> false 
+// "raunak"=> true
+ 
+
+ let someNumber=33
+  
+ let stringNumber=String(someNumber)
+ console.log(someNumber)    
+ console.log(typeof stringNumber)    
+
+

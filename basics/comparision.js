8b2bdc793b14d8a2d7c9d9f6cc9c3787c934a77a
Raunak11-tandon <raunaktandon11@gmail.com>
2026-06-15 05:49:40 +0000

    comparision between operators

---
+++ basics/comparision.js
@@ -0,0 +1,22 @@
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+
+
+// console.log( "2" > 1) 
+// console.log( "02" > 1) 
+console.log( null > 0) 
+console.log( null == 0) 
+console.log( null >= 0) 
+ // there is difference in equality chekc and comparision operator in js 
+  // comparisions operator work different they assign 0 to null 
+  console.log(undefined==0);
+
+  // avoid these type of codes bca of confusion  
+
+  console.log('2'===2);
+  
+   

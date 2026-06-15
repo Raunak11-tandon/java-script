12437f85a31fedb2084b0042c679cc718e2148e9
Raunak11-tandon <raunaktandon11@gmail.com>
2026-06-15 05:08:50 +0000

    conersions updated

--- basics/conversion.js
+++ basics/conversion.js
@@ -28,6 +28,36 @@ let booleanloggedIn=Boolean(loggedIn)
   
  let stringNumber=String(someNumber)
  console.log(someNumber)    
- console.log(typeof stringNumber)    
+ console.log(typeof stringNumber)   
+   // *****************operations*******************************
+//  let value =3 
+//  let negVaLUE= -value 
+//  console.log(negVaLUE)  
 
+//  console.log(2+2)
+//  console.log(2*2)
+//  console.log(2**3)
+//  console.log(2-2)
+//  console.log(2/2)
+//  console.log(2%3)
 
+//   let str1 = "raunak" 
+// let str2=" tandon" 
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log(1+"2") 
+// console.log("1" +2) 
+// console.log("1" +2 + 2)  
+// console.log(1+2+"2")  
+
+console.log(true)
+console.log(+true)
+console.log(+" ") 
+
+let  num1,num2,num3
+ num1=num2=num3= 2+2 
+
+  let gamecounter =100 
+  ++gamecounter;  
+  console.log(gamecounter)

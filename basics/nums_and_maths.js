71c5490f5c5a525276870e961c3490f3b357c730
Raunak11-tandon <raunaktandon11@gmail.com>
2026-06-21 07:45:26 +0000

    numbers and maths

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,33 @@
+const score =400 
+//  console.log(score);
+ 
+const balance = new Number(100)
+// console.log(balance)     
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+      
+ const othervalue=123.8977 
+//  console.log(othervalue.toPrecision(2));
+  
+ const hundred=100000 
+//  console.log(hundred.toLocaleString('en-IN')); 
+
+
+//  ++++++++++++++++++++++++++++++++maths+++++++++++++++++++++++++++++++++++
+    
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.8));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,3,4,2));
+// console.log(Math.max(4,6,8,3));
+// console.log(Math.random()) // between 0 and 1  
+// console.log(Math.random()*10)  
+// console.log((Math.floor(Math.random()*10))+1)  
+
+const min=10
+const max=20
+
+
+console.log(Math.floor(Math.random() * (max-min+1)) +min)
